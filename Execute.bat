30d5261f360739b3e40742b602dcd18be9b86ef7
praspeshve <praspeshve@gmail.com>
2019-04-25 18:08:31 +0530

    second commit

---
+++ Execute.bat
@@ -0,0 +1,2 @@
+set classpath=E:\Scenarios\svg\software\*;E:\Scenarios\svg\bin
+java org.testng.TestNG testng.xml
--- run.bat
+++
@@ -1,2 +0,0 @@
-set classpath=EScenariossvgsoftware;EScenariossvgbin
-java org.testng.TestNG testng.xml
